2e41e3ca7d36ac5229d5e694ec1c27c0d7d345e6
Nuntius Creative Hub <hola@nuntius.dev>
2024-11-25 15:59:42 -0500

    Update start_services.sh

--- start_services.sh
+++ start_services.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-# Iniciar Flask en segundo plano
 python app.py &
-
-# Iniciar Streamlit en segundo plano
-streamlit run streamlit_app.py --server.port 8501 --server.address 0.0.0.0
+streamlit run streamlit_app.py --server.port 8501 --server.address 0.0.0.0 &
+wait
